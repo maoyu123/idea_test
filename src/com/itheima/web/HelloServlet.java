7d7555d90c8b5b8fbacaf6e861d45027b8be57ab
maoyu <763935210@qq.com>
2018-02-01 22:04:12 +0800

    init

---
+++ src/com/itheima/web/HelloServlet.java
@@ -0,0 +1,13 @@
+package com.itheima.web;
+
+import java.io.IOException;
+
+public class HelloServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.getWriter().write("hello,jmy");
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        doPost(request,response);
+    }
+}
